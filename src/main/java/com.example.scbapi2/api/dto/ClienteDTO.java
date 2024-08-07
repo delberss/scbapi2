826bd8f74802ec7105eb2c6ea9d2f014c4c189e9
delberss <delberss@hotmail.com>
2024-08-06 22:24:01 -0300

    retira atributo e lista de contas de Cliente

--- src/main/java/com.example.scbapi2/api/dto/ClienteDTO.java
+++ src/main/java/com.example.scbapi2/api/dto/ClienteDTO.java
@@ -1,13 +1,10 @@
 package com.example.scbapi2.api.dto;
 
 import com.example.scbapi2.model.entity.Cliente;
-import com.example.scbapi2.model.entity.Conta;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
 
 @Data
 @NoArgsConstructor
@@ -20,22 +17,12 @@ public class ClienteDTO {
     private String email;
     private String telefone;
     private EnderecoDTO endereco;
-    private List<Long> contas; // Apenas IDs das contas
 
     public static ClienteDTO create(Cliente cliente) {
-        ClienteDTO dto = new ClienteDTO();
-        dto.setId(cliente.getId());
-        dto.setNome(cliente.getNome());
-        dto.setCpf(cliente.getCpf());
-        dto.setEmail(cliente.getEmail());
-        dto.setTelefone(cliente.getTelefone());
+        ModelMapper modelMapper = new ModelMapper();
+        ClienteDTO dto = modelMapper.map(cliente, ClienteDTO.class);
         if (cliente.getEndereco() != null) {
             dto.setEndereco(EnderecoDTO.create(cliente.getEndereco()));
-        }
-        if (cliente.getContas() != null) {
-            dto.setContas(cliente.getContas().stream()
-                    .map(Conta::getId)
-                    .collect(Collectors.toList()));
         }
         return dto;
     }
--- src/main/java/com.example.scbapi2/model/entity/Cliente.java
+++ src/main/java/com.example.scbapi2/model/entity/Cliente.java
@@ -1,21 +1,16 @@
 package com.example.scbapi2.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Cliente extends Pessoa {
-
-    @OneToMany(mappedBy = "cliente")
-    @JsonIgnore // Ignora a serialização recursiva
-    private List<Conta> contas;
+    // @OneToMany(mappedBy = "cliente")
+    //    @JsonIgnore // Ignora a serialização recursiva
+    //    private List<Conta> contas;
+    //}
 }
