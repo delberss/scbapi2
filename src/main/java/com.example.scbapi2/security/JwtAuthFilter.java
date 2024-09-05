c481031ebd02fad322587f7975f59fb83e05e565
Luiz Guilherme <37851595+Luiizrst@users.noreply.github.com>
2024-09-05 20:04:16 -0300

    JwtAuthFilter

--- src/main/java/com.example.scbapi2/security/JwtAuthFilter.java
+++ src/main/java/com.example.scbapi2/security/JwtAuthFilter.java
@@ -13,6 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private UsuarioService usuarioService;
+
+    public JwtAuthFilter( JwtService jwtService, UsuarioService usuarioService ) {
+        this.jwtService = jwtService;
+        this.usuarioService = usuarioService;
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest httpServletRequest,
