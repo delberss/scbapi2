84f5cc3e726063ff44f97c17e988e118dee40ed3
Luiz Guilherme <37851595+Luiizrst@users.noreply.github.com>
2024-09-09 13:59:45 -0300

    teste

--- src/main/java/com.example.scbapi2/exception/SenhaInvalidaException.java
+++ src/main/java/com.example.scbapi2/exception/SenhaInvalidaException.java
@@ -1,7 +1,1 @@
-package com.example.scbapi2.exception;
 
-public class SenhaInvalidaException extends RuntimeException {
-    public SenhaInvalidaException() {
-        super("Senha inválida");
-    }
-}
